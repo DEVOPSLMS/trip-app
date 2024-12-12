3a0e726ae0cf1b80ea5b58f703f2c895fc5cb1c7
DEVOPSLMS <2001642D@student.tp.edu.sg>
2024-12-12 19:44:06 +0800

    Added Server file plus folders

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './component/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' }, // Redirect root to 'home'
+  { path: 'home', component: HomeComponent, pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
